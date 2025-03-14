28e23302f6944996df0787c5c195f8db5cfd7f21
JoaoMartins07 <132686796+JoaoMartins07@users.noreply.github.com>
2025-03-14 10:20:53 +0000

    Update CategoriasInfo.vue

--- frontend/src/components/CategoriasInfo.vue
+++ frontend/src/components/CategoriasInfo.vue
@@ -1,86 +1,87 @@
 <template>
-   
-    <h1 v-if="infocategoria">{{ infocategoria.titulo }}</h1>
-    <h1>Info da Categoria</h1>
-
+    <h1 v-if="produto">{{ produto.nome }}</h1>
+    <h1>Info do Produto</h1>
 
     <menuzone />
 
-
     <footer class="footer_section">
         <div class="container">
-        <div class="row">
-
-
-            <div class="col-md-4 footer-col">
-                <div class="footer_detail">
-                    <p>
-                        ID #
-                        <br>
-                        {{ infocategoria.id }}
-                    </p>
+            <div class="row">
+                <div class="col-md-4 footer-col">
+                    <div class="footer_detail">
+                        <p>
+                            ID #
+                            <br>
+                            {{ produto.id }}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <div class="footer_detail">
+                        <p>
+                            IDCATEGORIA #
+                            <br>
+                            {{ produto.idcategoria }}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <div class="footer_detail">
+                        <p>
+                            NOME
+                            <br>
+                            {{ produto.nome }}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <div class="footer_detail">
+                        <p>
+                            PREÇO
+                            <br>
+                            {{ produto.preco }} €
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <div class="footer_detail">
+                        <p>
+                            QUANTIDADE
+                            <br>
+                            {{ produto.qtd }} 
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-4 footer-col">
-                <div class="footer_detail">
-                    <p>
-                        ORDEM
-                        <br>
-                        {{ infocategoria.ordem }}
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-4 footer-col">
-                <div class="footer_detail">
-                    <p>
-                        TITULO
-                        <br>
-                        {{ infocategoria.titulo }}
-                    </p>
-                </div>
-            </div>            
         </div>
-
-
-        </div>
-    </footer>  
-
-
+    </footer>
 </template>
 
-
 <style scoped>
-    h1 {text-align:center; color: #fff; margin:50px 10px;}
+h1 {
+    text-align: center;
+    color: #fff;
+    margin: 50px 10px;
+}
 </style>
 
-
 <script>
-
-
 import Menu from './Menu.vue';
-
 
 export default {
     data() {
         return {
-            infocategoria: this.fetchData(),
-            dataX: this.fetchDataX(),
+            produto: this.fetchData(),
         };
     },
     methods: {
-        async fetchData() {      
-            const response = await fetch("http://localhost:3000/categorias/" + this.$route.params.id);
-            this.infocategoria = await response.json();
-        },
-        async fetchDataX() {      
-            const response = await fetch("http://localhost:3000/categorias/" + this.$route.params.id);
-            this.dataX = await response.json();
+        async fetchData() {
+            const response = await fetch("http://localhost:3000/produtos/" + this.$route.params.idproduto);
+            this.produto = await response.json();
         }
-
-
     },
     components: {
         'menuzone': Menu,
-    }      
+    }
 };
 </script>
